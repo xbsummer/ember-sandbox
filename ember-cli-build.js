1c4ae58748114de85d85ee6ae57a369a104d8ddf
nightire <nightire@very-geek.com>
2016-09-23 01:01:03 +0800

    😊 Bravo! Let's Begin!
    Signed-off-by: nightire <nightire@very-geek.com>

---
+++ ember-cli-build.js
@@ -0,0 +1,38 @@
+var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function(defaults) {
+  var app = new EmberApp(defaults, {
+    cssModules: {
+      plugins: [
+        require('postcss-import'),
+        require('postcss-extend'),
+        require('postcss-cssnext')
+      ],
+      virtualModules: {
+        'ui-colors': {
+          'ui-red': '#f44336',
+          'ui-pink': '#e91e63',
+          'ui-purple': '#9c27b0',
+          'ui-deep-purple': '#673ab7',
+          'ui-indigo': '#3f51b5',
+          'ui-blue': '#2196f3',
+          'ui-light-blue': '#03a9f4',
+          'ui-cyan': '#00bcd4',
+          'ui-teal': '#009688',
+          'ui-green': '#4caf50',
+          'ui-light-green': '#8bc34a',
+          'ui-lime': '#cddc39',
+          'ui-yellow': '#ffeb3b',
+          'ui-amber': '#ffc107',
+          'ui-orange': '#ff9800',
+          'ui-deep-orange': '#ff5722',
+          'ui-brown': '#795548',
+          'ui-gray': '#9e9e9e',
+          'ui-blue-gray': '#607d8b'
+        }
+      }
+    }
+  });
+
+  return app.toTree();
+};
