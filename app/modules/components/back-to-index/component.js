1a73d296625e6584e5e31d871d4f5470c8b8ae13
nightire <nightire@very-geek.com>
2016-09-28 19:39:50 +0800

    Add loading template for all 1st level child routes

--- app/modules/components/back-to-index/component.js
+++ app/modules/components/back-to-index/component.js
@@ -1,4 +1,10 @@
 import Component from 'ember-component';
+import computed from 'ember-computed';
+import get from 'ember-metal/get';
 
 export default Component.extend({
+  shouldReveal: computed('currentRouteName', function() {
+    const routeName = get(this, 'currentRouteName');
+    return !(routeName == 'loading' || routeName == 'index');
+  })
 }).reopenClass({ positionalParams: ['currentRouteName'] });
